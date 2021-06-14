daa1067da5d2baadb8a83e838f18b0a0f9681eba
iwishiwasaneagle <jh.ewers@gmail.com>
2021-06-14 21:53:28 +0100

    Lovely data models

--- models/SpotifyAuthDetails.py
+++ models/SpotifyAuthDetails.py
@@ -1,4 +1,6 @@
 from os import access
+from typing import Any
+import utils
 from pydantic import BaseModel
 from pydantic.fields import Field
 
@@ -7,10 +9,21 @@ class SpotifyAuthDetailsFields:
     access_token = Field(description="Access token")
 
     refresh_token = Field(description="Refresh token")
+
+    expires_in = Field(description="Expires in X seconds")
+
+    expires_at = Field(-1,description="Expires at")
  
 
-class SpotifyAuthDetails(BaseModel):
-    
+    token_type = Field(description='Token type')
+
+class SpotifyAuthDetails(BaseModel):    
     access_token: str = SpotifyAuthDetailsFields.access_token
 
     refresh_token: str = SpotifyAuthDetailsFields.refresh_token
+
+    expires_in: int = SpotifyAuthDetailsFields.expires_in
+
+    expires_at: int = SpotifyAuthDetailsFields.expires_at
+
+    token_type: str = SpotifyAuthDetailsFields.token_type
