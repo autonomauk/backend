8b87031de937d1ac121cd93a5b3dc7b829fe8f6f
iwishiwasaneagle <jh.ewers@gmail.com>
2021-06-14 21:52:39 +0100

    CLI structure

---
+++ __main__.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(prog='SpotifyPlaylister', description='SpotifyPlaylister CLI')
+parser.add_argument('--worker', action='store_true')
+parser.add_argument('--server', action='store_true')
+
+parser.add_argument("--env", choices=['production','development'], default='development')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    os.environ['SP_ENV'] = args.env
+
+    if args.server:
+        import uvicorn
+        from config import PORT
+        uvicorn.run("api:app", host="127.0.0.1", port=PORT)
+        
+    elif args.worker:
+        from workers.SpotiCron import SpotiCron
+        SpotiCron()
+
+    else:
+        parser.print_help()
