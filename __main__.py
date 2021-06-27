0e8f8d6aa9da4489340b20bb086a008dcbd83d71
iwishiwasaneagle <jh.ewers@gmail.com>
2021-06-27 18:10:55 +0100

    Dev Env specific items, general import

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import uvicorn
 
 parser = argparse.ArgumentParser(prog='SpotifyPlaylister', description='SpotifyPlaylister CLI')
 parser.add_argument('--worker', action='store_true')
@@ -13,13 +14,10 @@ if __name__ == "__main__":
     os.environ['SP_ENV'] = args.env
 
     if args.server:
-        import uvicorn
-        from config import PORT
-        uvicorn.run("api:app", host="127.0.0.1", port=PORT, root_path='/api')
-        
+        from config import PORT, UVICORN_ROOT_PATH
+        uvicorn.run("api:app", host="127.0.0.1", port=PORT, root_path=UVICORN_ROOT_PATH, reload=args.env=="development")
     elif args.worker:
         from workers.SpotiCron import SpotiCron
         SpotiCron()
-
     else:
         parser.print_help()
