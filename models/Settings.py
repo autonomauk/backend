14516af46e44a325c4380b7536215116bdf7c68b
iwishiwasaneagle <jh.ewers@gmail.com>
2021-06-27 18:12:11 +0100

    Settings

---
+++ models/Settings.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from pydantic.fields import Field 
+from enum import Enum
+
+class PlaylistNamingSchemeEnum(str,Enum):
+    MONTHLY = "MONTHLY"
+
+class Settings(BaseModel):
+    class Config:
+        use_enum_values= True
+
+    PlaylistNamingScheme = Field(default=PlaylistNamingSchemeEnum.MONTHLY,description="The playlist naming scheme")
+    
