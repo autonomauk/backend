b5494305b6234fd9925e7fd03b61203958c17174
iwishiwasaneagle <jh.ewers@gmail.com>
2021-08-14 20:35:03 +0100

    Publish build info to prometheus

--- repositories/stats.py
+++ repositories/stats.py
@@ -1,5 +1,8 @@
 import prometheus_client
 
+from config import VERSION
+
+
 from models.Stats import RunTimeStat, UserCreationStat, UserDeletionStat
 
 SPOTICRON_RUN_TIME = prometheus_client.Histogram('spoticron_run_time', 'Total time taken for spoticron to run', buckets=(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,2.0,5.0,10.0,float('inf')))
@@ -10,6 +13,9 @@ AUTONOMA_USER_DELETED = prometheus_client.Counter('autonoma_api_user_deleted', '
 SPOTICRON_SPOTIFY_REQUESTS = prometheus_client.Counter("spoticron_spotify_request", 'Track total spotify requests')
 SPOTICRON_DISABLED = prometheus_client.Counter("spoticron_disabled", "Number of times accounts have disabled spoticron")
 SPOTICRON_ENABLED = prometheus_client.Counter("spoticron_enabled", "Number of times accounts have enabled spoticron")
+
+AUTONOMA_BUILD = prometheus_client.Info('autonoma_api_build', "Information about the backend build")
+AUTONOMA_BUILD.info({'version':VERSION})
 
 class StatsRepository:
     @staticmethod
