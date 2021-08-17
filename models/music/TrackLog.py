9ff181e54b06ad9125aa0724b6efc274410ba564
iwishiwasaneagle <jh.ewers@gmail.com>
2021-08-17 12:21:11 +0100

    TrackLog pagination

--- models/music/TrackLog.py
+++ models/music/TrackLog.py
@@ -1,3 +1,4 @@
+from pydantic.main import BaseModel
 from models.ListModel import ListModel
 from models.music import Track,Playlist
 from models.TimedBaseModel import TimedBaseModel
@@ -7,3 +8,9 @@ class TrackLog(TimedBaseModel):
     playlist: Playlist
 
 TrackLogs = list[TrackLog]
+
+class TrackLogReturnPaginated(BaseModel):
+    track_log: TrackLogs
+    offset: int
+    length: int
+    total: int
--- routers/me/track_log/track_log.py
+++ routers/me/track_log/track_log.py
@@ -1,4 +1,4 @@
-from models.music.TrackLog import TrackLogs
+from models.music.TrackLog import TrackLogReturnPaginated, TrackLogs
 from models.ObjectId import PydanticObjectId
 from repositories.user import UserRepository
 from repositories.auth_flow import AuthFlowRepository
@@ -10,7 +10,7 @@ router = APIRouter(
     tags=['track log']
 )
 
-@router.get('/',response_model=TrackLogs)
-def track_log(id: str = Depends(AuthFlowRepository.auth_required_dep)) -> TrackLogs:
-    track_log: TrackLogs = UserRepository.read_track_log(PydanticObjectId(id))
-    return track_log
+@router.get('/',response_model=TrackLogReturnPaginated)
+def track_log(id: str = Depends(AuthFlowRepository.auth_required_dep), offset: int = 0, length: int = 5) -> TrackLogReturnPaginated:
+    track_log, total = UserRepository.read_track_log(PydanticObjectId(id), offset, length)
+    return {"track_log":track_log,"total":total,"offset":offset,"length":len(track_log)}
