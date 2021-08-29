9023d37cedd23621accf29dde0f51c05272cf073
iwishiwasaneagle <jh.ewers@gmail.com>
2021-08-29 13:26:56 +0100

    Use labels correctly

--- repositories/stats.py
+++ repositories/stats.py
@@ -7,7 +7,7 @@ from models.Stats import RunTimeStat, UserCreationStat, UserDeletionStat
 
 SPOTICRON_RUN_TIME = prometheus_client.Histogram('spoticron_run_time', 'Total time taken for spoticron to run', buckets=(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,2.0,5.0,10.0,float('inf')))
 SPOTICRON_RUNS = prometheus_client.Counter('spoticron_runs', 'Total spoticron runs')
-SPOTICRON_TRACK_ADDED = prometheus_client.Counter('spoticron_tracks_added', 'Total tracks added to playlists')
+SPOTICRON_TRACK_ADDED = prometheus_client.Counter('spoticron_tracks_added', 'Total tracks added to playlists',['id'])
 AUTONOMA_USER_CREATED = prometheus_client.Counter('autonoma_api_user_created', 'Total times an account has been created')
 AUTONOMA_USER_DELETED = prometheus_client.Counter('autonoma_api_user_deleted', 'Total times an account has been deleted')
 SPOTICRON_SPOTIFY_REQUESTS = prometheus_client.Counter("spoticron_spotify_request", 'Track total spotify requests')
@@ -49,5 +49,5 @@ class StatsRepository:
     def spoticron_disabled() -> None:
         SPOTICRON_DISABLED.inc()
 
-    def spoticron_tracks_added(n:int = 1) -> None:            
-        SPOTICRON_TRACK_ADDED.inc(n)
+    def spoticron_tracks_added(n:int = 1, id = None) -> None:
+        SPOTICRON_TRACK_ADDED.label(id=id).inc(n)
